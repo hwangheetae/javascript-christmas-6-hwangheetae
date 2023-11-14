b830bd32b277a78fafc5048db172bf9cfa7b99c7
hwangheetae <tejaia@naver.com>
2023-11-14 19:57:57 +0900

    feat: 메뉴판에 없는 음식 주문 에러체크 orderMessageCheck() 추가

--- src/ErrorCheck.js
+++ src/ErrorCheck.js
@@ -5,6 +5,42 @@ class ErrorCheck {
       throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     }
   }
+
+  static orderMessageCheck(input) {
+    const MENU = {
+      //category
+      애피타이저: {
+        //name       //price
+        양송이수프: 6000,
+        타파스: 5500,
+        시저샐러드: 8000,
+      },
+      메인: {
+        티본스테이크: 55000,
+        바비큐립: 54000,
+        해산물파스타: 35000,
+        크리스마스파스타: 25000,
+      },
+      디저트: {
+        초코케이크: 15000,
+        아이스크림: 5000,
+      },
+      음료: {
+        제로콜라: 3000,
+        레드와인: 60000,
+        샴페인: 25000,
+      },
+    };
+
+    const MENU_NAME_LIST = Object.values(MENU).reduce((acc, category) => {
+      return acc.concat(Object.keys(category));
+    }, []);
+
+    const isValidOrder = input.every((item) => MENU_NAME_LIST.includes(item));
+    if (!isValidOrder) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+  }
 }
 
 export default ErrorCheck;
--- src/InputView.js
+++ src/InputView.js
@@ -18,9 +18,29 @@ const InputView = {
   },
 
   async readMenu() {
-    const input = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
-    );
+    while (true) {
+      const input = await Console.readLineAsync(
+        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+      );
+      const menuItems = input.split(',');
+      const menu = {};
+
+      menuItems.forEach((item) => {
+        const [menuItem, quantity] = item.split('-');
+        menu[menuItem.trim()] = parseInt(quantity);
+      });
+
+      const menuName = Object.keys(menu);
+
+      try {
+        ErrorCheck.orderMessageCheck(menuName);
+        return input;
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+
     return input;
   },
 };
