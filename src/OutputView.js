834f71e71ce4eb540307b1f20236b51692e3a992
hwangheetae <tejaia@naver.com>
2023-11-14 17:18:00 +0900

    feat: 메뉴 출력하기

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,22 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  printMenu(input) {
+    Console.print('<주문 메뉴>');
+    const menuItems = input.split(',');
+    const menu = {};
+
+    menuItems.forEach((item) => {
+      const [menuItem, quantity] = item.split('-');
+      menu[menuItem.trim()] = parseInt(quantity);
+    });
+
+    const menuKey = Object.keys(menu);
+    const menuQuantity = Object.values(menu);
+    for (let i = 0; i < menuKey.length; i += 1) {
+      Console.print(`${menuKey[i]} ${menuQuantity[i]}개`);
     }
-    // ...
-}
+  },
+};
+
+export default OutputView;
