3b73e07ba9f8afad31018e6c6c5dad84941a4075
hwangheetae <tejaia@naver.com>
2023-11-14 23:45:53 +0900

    style: 출력스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import InputView from './InputView.js';
 import OutputView from './OutputView.js';
 import EventAlgorithm from './EventAlgorithm.js';
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
     const date = await InputView.readDate();
     EventAlgorithm.determiningDate(date);
     const menu = await InputView.readMenu();
+    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n');
     OutputView.printMenu(menu);
   }
 }
