411512b2d0704cbb87584bf83b60b5b4f69cc1cd
hwangheetae <tejaia@naver.com>
2023-11-14 21:36:28 +0900

    fix: orderFormatCheck() 버그 수정

--- src/ErrorCheck.js
+++ src/ErrorCheck.js
@@ -52,7 +52,7 @@ class ErrorCheck {
   }
 
   static orderFormatCheck(input) {
-    const pattern = /^.+-.+(,.+-.+)*$/;
+    const pattern = /^.+-\d+(,.+-.+)*$/;
     if (!pattern.test(input)) {
       throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
