9fb7e771990b48e17c0e8cd32e3f1c02e92e75b7
hwangheetae <tejaia@naver.com>
2023-11-15 21:57:13 +0900

    refactor: 상수분리

---
+++ src/Constant.js
@@ -0,0 +1,8 @@
+export const INVALID_DATE_MESSAGE =
+  '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.';
+export const INVALID_ORDER_MESSAGE =
+  '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+export const INVALID_MAX_ORDER_MESSAGE =
+  '[ERROR] 메뉴는 20개까지만 주문할 수 있습니다. 다시 주문해 주세요';
+export const INVALID_BEVERAGE_ONLY_MESSAGE =
+  '[ERROR] 음료만 주문할 수 없습니다. 다른 메뉴를 추가해 주세요.';
--- src/ErrorCheck.js
+++ src/ErrorCheck.js
@@ -1,53 +1,56 @@
+import {
+  INVALID_DATE_MESSAGE,
+  INVALID_ORDER_MESSAGE,
+  INVALID_MAX_ORDER_MESSAGE,
+  INVALID_BEVERAGE_ONLY_MESSAGE,
+} from './Constant.js';
+
 class ErrorCheck {
   static dateCheck(input) {
     input = Number(input);
     if (input > 31 || input < 1 || isNaN(input)) {
-      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+      throw new Error(INVALID_DATE_MESSAGE);
     }
   }
 
   static orderMenuNameCheck(event_algorithm, input) {
     const isValidNameOrder = event_algorithm.findOrderInMenu(input);
     if (!isValidNameOrder) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(INVALID_ORDER_MESSAGE);
     }
   }
 
   static orderMenuNumberCheck(input) {
     input = Number(input);
     if (input < 1 || isNaN(input)) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(INVALID_ORDER_MESSAGE);
     }
   }
 
   static orderFormatCheck(input) {
     const pattern = /^.+-\d+(,.+-.+)*$/;
     if (!pattern.test(input)) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(INVALID_ORDER_MESSAGE);
     }
   }
 
   static orderOverlapCheck(input) {
     if (new Set(input).size !== input.length) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error(INVALID_ORDER_MESSAGE);
     }
   }
 
   static menuNumberMaxCheck(input) {
     const sum = input.reduce((a, b) => a + b, 0);
     if (sum > 20) {
-      throw new Error(
-        '[ERROR] 메뉴는 20개까지만 주문할 수 있습니다. 다시 주문해 주세요',
-      );
+      throw new Error(INVALID_MAX_ORDER_MESSAGE);
     }
   }
 
   static onlyBeverageCheck(event_algorithm, input) {
     const isOnlyBeverages = event_algorithm.orderBeverageOnly(input);
     if (isOnlyBeverages) {
-      throw new Error(
-        '[ERROR] 음료만 주문할 수 없습니다. 다른 메뉴를 추가해 주세요.',
-      );
+      throw new Error(INVALID_BEVERAGE_ONLY_MESSAGE);
     }
   }
 }
