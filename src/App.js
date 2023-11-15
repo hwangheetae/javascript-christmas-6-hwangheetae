119b315cdf77c08c9b33fb77e86d06eabeb49c8e
hwangheetae <tejaia@naver.com>
2023-11-15 21:58:05 +0900

    refactor: calculateBenefitDetail()에서 countCategory() 분리

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    OutputView.printGreeting();
     const event_algorithm = new EventAlgorithm();
+
+    OutputView.printGreeting();
+
     const date = await InputView.readDate();
     const menu = await InputView.readMenu(event_algorithm);
+
     event_algorithm.determiningDate(date);
+
     OutputView.printGuideWord(date);
     OutputView.printMenu(event_algorithm, menu, date);
   }
--- src/EventAlgorithm.js
+++ src/EventAlgorithm.js
@@ -61,19 +61,15 @@ class EventAlgorithm {
 
   determiningDate(date) {
     // 1~25일
-    // 총주문금액 - (1000 +100 x (day -1))
     const CHRISTMAS_EVENT_DAY = Array.from({ length: 25 }, (_, i) => i + 1);
 
-    // => 메인메뉴 1개당 2023할인
     const WEEKEND = [1, 2, 8, 9, 15, 16, 22, 23, 29, 30];
 
     // [3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31]
-    // => 디저트메뉴 1개당 2023원 할인
     const WEEKDAY = Array.from({ length: 31 }, (_, i) => i + 1).filter(
       (x) => !WEEKEND.includes(x),
     );
 
-    // => 총 주문 금액 - 1000원 할인
     const SPECIAL_EVENT_DAY = [3, 10, 17, 24, 25, 31];
 
     if (CHRISTMAS_EVENT_DAY.includes(Number(date))) {
@@ -160,22 +156,12 @@ class EventAlgorithm {
       }
 
       if (this.IS_WEEKEND) {
-        let count = 0;
-        menuNameList.forEach((item) => {
-          if (this.findCategoryOfMenu(item) === '메인') {
-            count += menu[item];
-          }
-        });
+        const count = this.countCategory(menuNameList, menu, '메인');
         weekend_discount = -2023 * count;
       }
 
       if (this.IS_WEEKDAY) {
-        let count = 0;
-        menuNameList.forEach((item) => {
-          if (this.findCategoryOfMenu(item) === '디저트') {
-            count += menu[item];
-          }
-        });
+        const count = this.countCategory(menuNameList, menu, '디저트');
         weekday_discount = -2023 * count;
       }
 
@@ -195,6 +181,16 @@ class EventAlgorithm {
       special_discount,
       giftmenu_discount,
     };
+  }
+
+  countCategory(menuNameList, menu, category) {
+    let count = 0;
+    menuNameList.forEach((item) => {
+      if (this.findCategoryOfMenu(item) === category) {
+        count += menu[item];
+      }
+    });
+    return count;
   }
 
   findCategoryOfMenu(menuItem) {
