d82cddcb6b8de7589d52c4ce57d185ee535abc99
hwangheetae <tejaia@naver.com>
2023-11-15 19:42:48 +0900

    feat: 출력 완성

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
+    OutputView.printGreeting();
     const event_algorithm = new EventAlgorithm();
     const date = await InputView.readDate();
     const menu = await InputView.readMenu(event_algorithm);
     event_algorithm.determiningDate(date);
     OutputView.printGuideWord(date);
-    OutputView.printMenu(event_algorithm, menu);
+    OutputView.printMenu(event_algorithm, menu, date);
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
+  printGreeting() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+  },
+
   printGuideWord(date) {
     Console.print(
       `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
     );
   },
 
-  printMenu(event_algorithm, input) {
+  printMenu(event_algorithm, input, date) {
     const { menu, menuName, menuQuantity, menuNameList } =
       event_algorithm.userOrder(input);
 
@@ -23,7 +27,19 @@ const OutputView = {
     );
 
     this.printGiftMenu(Number(event_algorithm.IS_GIFT_MENU));
-    this.printBenefitsDetail(event_algorithm);
+    const total_discount = this.printBenefitsDetail(
+      event_algorithm,
+      date,
+      menuNameList,
+      menu,
+    );
+    this.printTotalDiscount(total_discount);
+    this.printFinalPrice(
+      event_algorithm.IS_GIFT_MENU,
+      totalPriceBeforeDisCount,
+      total_discount,
+    );
+    this.printEventBadge(total_discount);
   },
 
   printOrder(menuName, menuQuantity) {
@@ -51,15 +67,84 @@ const OutputView = {
     }
   },
 
-  printBenefitsDetail(event_algorithm) {
+  printBenefitsDetail(event_algorithm, date, menuNameList, menu) {
+    let total_discount = 0;
+
     Console.print('<혜택 내역>');
-    Console.print(`크리스마스${event_algorithm.isChristmasEventDay}`);
-    Console.print(`주말${event_algorithm.isWeekend}`);
-    Console.print(`평일${event_algorithm.isWeekday}`);
-    Console.print(`스페셜이벤트${event_algorithm.isSpecialEvent}`);
-    Console.print(`증정${event_algorithm.isGiftMenu}`);
-    Console.print(`이벤트뱃지${event_algorithm.isEventBadge}`);
+
+    const {
+      christmas_discount,
+      weekday_discount,
+      weekend_discount,
+      special_discount,
+      giftmenu_discount,
+    } = event_algorithm.calculateBenefitDetail(date, menu, menuNameList);
+
+    if (christmas_discount != 0) {
+      Console.print(
+        `크리스마스 디데이 할인: ${christmas_discount.toLocaleString()}원`,
+      );
+      total_discount += christmas_discount;
+    }
+
+    if (weekday_discount != 0) {
+      Console.print(`평일 할인: ${weekday_discount.toLocaleString()}원`);
+      total_discount += weekday_discount;
+    }
+
+    if (weekend_discount != 0) {
+      Console.print(`주말 할인: ${weekend_discount.toLocaleString()}원`);
+      total_discount += weekend_discount;
+    }
+
+    if (special_discount != 0) {
+      Console.print(`특별 할인: ${special_discount.toLocaleString()}원`);
+      total_discount += special_discount;
+    }
+
+    if (giftmenu_discount != 0) {
+      Console.print(`증정이벤트: ${giftmenu_discount.toLocaleString()}원`);
+      total_discount += giftmenu_discount;
+    }
+
+    if (
+      christmas_discount === 0 &&
+      weekday_discount === 0 &&
+      weekend_discount === 0 &&
+      special_discount === 0 &&
+      giftmenu_discount === 0
+    ) {
+      Console.print('없음');
+    }
+
+    return total_discount;
+  },
+
+  printTotalDiscount(total_discount) {
+    Console.print('\n<총혜택 금액>');
+    Console.print(`${total_discount.toLocaleString()}원\n`);
+  },
+
+  printFinalPrice(isWine, totalPriceBeforeDisCount, total_discount) {
+    Console.print('<할인 후 예상 결제 금액>');
+    let FINAL_PRICE = totalPriceBeforeDisCount + total_discount;
+    if (isWine) {
+      FINAL_PRICE += 25000;
+    }
+    Console.print(`${FINAL_PRICE.toLocaleString()}원\n`);
+  },
+
+  printEventBadge(total_discount) {
+    total_discount *= -1;
+    Console.print('<12월 이벤트 배지>');
+    if (total_discount >= 20000) Console.print('산타');
+    else if (total_discount >= 10000) Console.print('트리');
+    else if (total_discount >= 5000) Console.print('별');
+    else Console.print('없음');
   },
 };
 
 export default OutputView;
+// 5천이상: 별
+// 1만원 이상 : 트리
+// 2만원 이상 : 산타
